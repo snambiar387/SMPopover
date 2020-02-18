6a735f3dc3c9f5edbfea26808e7a2ecf366fcd05
nambiar-sreehari <57889313+nambiar-sreehari@users.noreply.github.com>
2020-02-18 19:09:09 +0530

    Added framework project

---
+++ Source/SMPopover/SMPopover/SMPopover.h
@@ -0,0 +1,19 @@
+//
+//  SMPopover.h
+//  SMPopover
+//
+//  Created by Sreehari M Nambiar on 18/02/20.
+//  Copyright © 2020 Sreehari M Nambiar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SMPopover.
+FOUNDATION_EXPORT double SMPopoverVersionNumber;
+
+//! Project version string for SMPopover.
+FOUNDATION_EXPORT const unsigned char SMPopoverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SMPopover/PublicHeader.h>
+
+
---
+++ Source/SMPopover/SMPopover/SMPopover.swift
@@ -0,0 +1,16 @@
+//
+//  SMPopover.swift
+//  SMPopover
+//
+//  Created by Sreehari M Nambiar on 18/02/20.
+//  Copyright © 2020 Sreehari M Nambiar. All rights reserved.
+//
+
+import UIKit
+
+public class SMPopover: UIView {
+    
+    public func present(from source: UIView) {
+        print("present popover")
+    }
+}
